2db06001097e73d0d7a58e5a235bcf066fa80fcc
Luccas Andrade <jluccasgabriel@gmail.com>
2022-11-11 03:53:17 -0300

    UI Card

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -110,9 +110,14 @@ export function Home({ loggedInUser }) {
 		getData();
 	}, []);
 
+	const minimizeHandler = () => {
+		const card = document.querySelector('.card')
+		card.classList.contains('minimize')? card.classList.remove('minimize'): card.classList.add('minimize')
+	}
+
 	return (
 		<>
-			<div className="flex">
+			<div className="flex flex-col items-center">
 			<div className="w-[600px] max-w-[90%] border-r border-silver shadow-xl">
 				<h1 className="text-2xl text-platinum m-4 font-bold">{`Bem vindo, ${loggedInUser.name.split(' ')[0]}!`}</h1>
 				<TweetForm loggedInUser={loggedInUser} onSuccess={getData} />
@@ -131,17 +136,18 @@ export function Home({ loggedInUser }) {
 
 				</div>
 			<div className="m-2 flex flex-col w-[50%]">
-				<a href='https://www.github.com/luccasandrade' target='_blank'>
-					<div className="card w-40 flex flex-col items-center shadow-lg p-4 self-center border-0 rounded-md">
-						<span className="text-sm opacity-60">created by</span>
-						<span className="text-sm opacity-80">Luccas Andrade</span>
-						<img className="rounded-full mb-2 border-purple-700 w-32" src='https://www.github.com/luccasandrade.png'></img>
-						<svg height="32" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true" class="octicon octicon-mark-github v-align-middle">
-						<path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
-						</svg>
-						<span>Github</span>
+					<div className="card w-40 flex flex-col relative justify-center items-center shadow-lg p-4 self-center border-0 rounded-md">
+						<span className="self-start cursor-pointer absolute top-0 left-2" onClick={minimizeHandler}>___</span>
+						<a className="flex flex-col items-center" href='https://www.github.com/luccasandrade' target='_blank'>
+							<span className="text-sm opacity-60">created by</span>
+							<span className="text-sm opacity-80">Luccas Andrade</span>
+							<img className="rounded-full mb-2 border-purple-700 w-32" src='https://www.github.com/luccasandrade.png'></img>
+							<svg height="32" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true" class="octicon octicon-mark-github v-align-middle">
+							<path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+							</svg>
+							<span>Github</span>
+						</a>
 					</div>
-				</a>
 			</div>
 			</div>
 		</>
